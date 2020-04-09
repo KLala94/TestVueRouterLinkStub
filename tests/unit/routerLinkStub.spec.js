76715e069aa3b98d16f83d68c7eb5e3eea8ef924
k.lalasis <lalaklod@gmail.com>
2020-04-09 11:59:28 +0200

    al Router-links tested

--- tests/unit/routerLinkStub.spec.js
+++ tests/unit/routerLinkStub.spec.js
@@ -1,5 +1,4 @@
 import Menu from "@/views/Menu.vue";
-import First from "@/components/first.vue";
 import { mount, RouterLinkStub } from '@vue/test-utils'
 import flushPromises from "flush-promises";
 describe("Menu.vue", () => {
@@ -8,10 +7,47 @@ describe("Menu.vue", () => {
     const wrapper = mount(Menu, {
       stubs: {
         RouterLink: RouterLinkStub,
-        First
       }
     })
 
     await flushPromises();
-    expect(wrapper.find(RouterLinkStub).props().to).toBe('/first')
-  })});
+    expect(wrapper.find('#testFirst').props().to).toBe('/first')
+    expect(wrapper.find('#testFirst').text()).toBe('first')
+  });
+  test("click on second routes to ./second", async () => {
+    const wrapper = mount(Menu, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      }
+    })
+
+    await flushPromises();
+    expect(wrapper.find('#testSecond').props().to).toBe('/second')
+    expect(wrapper.find('#testSecond').text()).toBe('second')
+
+  });
+  test("click on third routes to ./third", async () => {
+    const wrapper = mount(Menu, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      }
+    })
+
+    await flushPromises();
+    expect(wrapper.find('#testThird').props().to).toBe('/third')
+    expect(wrapper.find('#testThird').text()).toBe('third')
+
+  })
+  test("click on Main routes to ./", async () => {
+    const wrapper = mount(Menu, {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      }
+    })
+
+    await flushPromises();
+    expect(wrapper.find('#testMain').props().to).toBe('/')
+    expect(wrapper.find('#testMain').text()).toBe('Main Page')
+
+  });
+});
