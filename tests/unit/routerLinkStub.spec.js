f9afb97f6dfcb9f38ea1ee9badbe951186e35351
k.lalasis <lalaklod@gmail.com>
2020-04-09 12:04:45 +0200

    DRY used
    dont repeat yourself, so i edited the RouterLinkStub.spec.js

--- tests/unit/routerLinkStub.spec.js
+++ tests/unit/routerLinkStub.spec.js
@@ -3,48 +3,30 @@ import { mount, RouterLinkStub } from '@vue/test-utils'
 import flushPromises from "flush-promises";
 describe("Menu.vue", () => {
 
+  const wrapper = mount(Menu, {
+    stubs: {
+      RouterLink: RouterLinkStub,
+    }
+  })
   test("click on first routes to ./first", async () => {
-    const wrapper = mount(Menu, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-      }
-    })
 
     await flushPromises();
     expect(wrapper.find('#testFirst').props().to).toBe('/first')
     expect(wrapper.find('#testFirst').text()).toBe('first')
   });
   test("click on second routes to ./second", async () => {
-    const wrapper = mount(Menu, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-      }
-    })
-
     await flushPromises();
     expect(wrapper.find('#testSecond').props().to).toBe('/second')
     expect(wrapper.find('#testSecond').text()).toBe('second')
 
   });
   test("click on third routes to ./third", async () => {
-    const wrapper = mount(Menu, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-      }
-    })
-
     await flushPromises();
     expect(wrapper.find('#testThird').props().to).toBe('/third')
     expect(wrapper.find('#testThird').text()).toBe('third')
 
   })
   test("click on Main routes to ./", async () => {
-    const wrapper = mount(Menu, {
-      stubs: {
-        RouterLink: RouterLinkStub,
-      }
-    })
-
     await flushPromises();
     expect(wrapper.find('#testMain').props().to).toBe('/')
     expect(wrapper.find('#testMain').text()).toBe('Main Page')
